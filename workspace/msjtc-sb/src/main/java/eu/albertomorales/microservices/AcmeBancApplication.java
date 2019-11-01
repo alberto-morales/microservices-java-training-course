7ed291092641b1215cbff89ae0d6b1eaf3dc11b8
Alberto Morales Morales <mail@albertomorales.eu>
2019-11-01 05:20:49 +0000

    first (java project) commit

---
+++ workspace/msjtc-sb/src/main/java/eu/albertomorales/microservices/AcmeBancApplication.java
@@ -0,0 +1,13 @@
+package eu.albertomorales.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeBancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeBancApplication.class, args);
+	}
+
+}
