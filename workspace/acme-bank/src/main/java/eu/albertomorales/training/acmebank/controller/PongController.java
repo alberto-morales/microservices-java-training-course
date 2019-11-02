373aca7afb74cd6ff33d85b11fd228a14af2ad44
Alberto Morales Morales <mail@albertomorales.eu>
2019-11-02 17:17:35 +0000

    JPA

--- workspace/acme-bank/src/main/java/eu/albertomorales/training/acmebank/controller/PongController.java
+++ workspace/acme-bank/src/main/java/eu/albertomorales/training/acmebank/controller/PongController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
 public class PongController {
 	
 private static final String pongMsg = "Pong Mr./Ms. %s!";
