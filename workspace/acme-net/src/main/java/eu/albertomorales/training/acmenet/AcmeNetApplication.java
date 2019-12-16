73bd25ea0a73ea550cacb50394e56d33d388732e
Alberto Morales Morales <mail@albertomorales.eu>
2019-12-16 20:58:50 +0000

    turning eureka off

--- workspace/acme-net/src/main/java/eu/albertomorales/training/acmenet/AcmeNetApplication.java
+++ workspace/acme-net/src/main/java/eu/albertomorales/training/acmenet/AcmeNetApplication.java
@@ -2,11 +2,11 @@ package eu.albertomorales.training.acmenet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableEurekaClient
+//@EnableFeignClients
+//@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"eu.albertomorales.training.acmenet"})
 public class AcmeNetApplication {
 
